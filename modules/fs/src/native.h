c449ce68622f58efceb578c98d5a056182af1e3b
Tobias Peirick <peirick@gmail.com>
2020-03-09 17:11:57 +0100

    Made module fs windows compatible

--- modules/fs/src/native.h
+++ modules/fs/src/native.h
@@ -1,5 +1,27 @@
 size_t os_get_homedir(char *to, size_t to_len);
 
+#if defined(_WIN32)
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <Shlobj.h>
+#pragma comment(lib, "Shell32.lib")
+size_t os_get_homedir(char *to, size_t to_len)
+{
+    char homedir[MAX_PATH];
+    if (SHGetFolderPathA(0, CSIDL_COMMON_APPDATA, NULL, SHGFP_TYPE_CURRENT, homedir) != S_OK){
+        return 0;       
+    }
+
+    size_t ll = strlen(homedir);
+    if (ll > to_len) {            
+        return 0;
+    } 
+
+    memcpy(to, homedir, ll);
+    return ll;    
+}
+#endif
+
 #ifdef __unix__
 
 
